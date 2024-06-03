a6b3763b63eb88c60a0ef8a9ac29004a2987b447
ZAVERYGO <Kozichnick@mail.ru>
2024-06-03 16:31:45 +0300

    добивил возможность получения user и page

--- user-service/src/main/java/com/kozich/finance/user_service/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/kozich/finance/user_service/service/impl/UserServiceImpl.java
@@ -1,38 +1,60 @@
 package com.kozich.finance.user_service.service.impl;
 
 
+import com.kozich.finance.user_service.mapper.UserMapper;
 import com.kozich.finance.user_service.service.api.UserService;
 import com.kozich.finance.user_service.core.dto.UserDTO;
 import com.kozich.finance.user_service.model.UserEntity;
 import com.kozich.finance.user_service.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
+@Service
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    @Override
-    public UserEntity get(UUID UUID, String page, String size) {
+    private final UserMapper userMapper;
 
-        return null;
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
-    public UserEntity getPage(UUID UUID) {
-        return null;
+    public UserEntity getById(UUID uuid) {
+        UserEntity userEntity = userRepository.findById(uuid).orElseThrow
+                (
+                    () -> new IllegalArgumentException("Пользователя не существует")
+                );
+        return userEntity;
     }
 
+    @Override
+    public Page<UserEntity> getPage(Integer page, Integer size) {
+        return userRepository.findAll(PageRequest.of(page, size));
+    }
+
+    @Transactional
     @Override
     public UserEntity create(UserDTO userDTO) {
         return null;
     }
 
+    @Transactional
     @Override
     public UserEntity update(UserDTO userDTO) {
         return null;
     }
 
+    @Transactional
     @Override
     public void delete(UUID uuid) {
 
